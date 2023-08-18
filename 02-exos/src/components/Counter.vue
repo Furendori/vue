ad592068550ca7bceeb9e6d3d2408d2a105e7d29
Julian Brasseur <brasseurjulian2@gmail.com>
2023-08-18 08:44:48 +0200

    Suite exos

---
+++ 02-exos/src/components/Counter.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import { ref } from "vue";
+
+    const props = defineProps(["start", "max"])
+    const count = ref(props.start)
+    const decrement = () => count.value--
+    const increment = () => count.value++
+</script>
+
+<template>
+    <div>
+        <button @click="increment">+</button>
+        <p>{{ count }}</p>
+        <button :on-click="decrement">-</button>
+    </div>
+</template>
